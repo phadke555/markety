4a44ea7bb071c9d4d90e3c212007da124daee6c5
Rohan Phadke <rohanphadke@gmail.com>
2024-06-26 15:43:38 -0700

    initialize ios app

---
+++ markety/markety/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  markety
+//
+//  Created by Rohan Phadke on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ markety/markety/marketyApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketyApp.swift
+//  markety
+//
+//  Created by Rohan Phadke on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct marketyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
